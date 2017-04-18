c0ef35d8dbf4e9c6905b629921138b3164ee363e
merisahakyan <sahakyan_m@bk.ru>
2017-04-18 22:11:35 +0400

    TwitterClient

---
+++ TwitterClient/TwitterClient/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+using System.Net.Http.Formatting;
+
+namespace TwitterClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Tweets model = null;
+            var client = new HttpClient();
+            var task = client.GetAsync("http://search.twitter.com/search.json?q=plurarsight")
+                .ContinueWith((taskwithresponce) =>
+            {
+                var responce = taskwithresponce.Result;
+                var readtask = responce.Content.ReadAsAsync<Tweets>();
+                readtask.Wait();
+                model = readtask.Result;
+            }
+            );
+            task.Wait();
+            return View(model.results);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TwitterClient/TwitterClient/Controllers/Tweets.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace TwitterClient.Controllers
+{
+    public class Tweets
+    {
+        public  Tweet [] results;
+    }
+
+    public class Tweet
+    {
+        [JsonProperty("from_user")]
+        public string username { get; set; }
+        [JsonProperty("text")]
+        public string tweettext { get; set; }
+    }
+}
